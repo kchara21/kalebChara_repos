57137f1f3e44d6411ffd86fa4e58e9956c1e78f2
Kaleb Chara Toala <kalebdavidchara@hotmail.com>
2022-08-27 23:45:53 -0500

    Creating mock.service.ts

---
+++ src/services/mock.service.ts
@@ -0,0 +1,43 @@
+import { AppDataSource } from "../data_source";
+import { Repository } from "../entities/Repository";
+import { request, Request, response, Response } from "express";
+
+interface Res {
+  id: number;
+  state:number;
+}
+
+export default class MockService {
+  static verificationState = async (req: Request, res: Response) => {
+    let stateCode: number = 603;
+    const repoDataSource = AppDataSource.getRepository(Repository);
+    let repos: Repository[] = [];
+    let repositories: Res[] = [];
+
+    try {
+      repos = await repoDataSource.find({ where: { status: "A" } });
+    } catch (e) {
+      res.status(404).json({ message: "Something goes wrong!" });
+    }
+
+    for (const repo of repos) {
+      stateCode++;
+      repositories.push({ id: repo.id_repository, state: stateCode });
+      if (stateCode > 605) {
+        stateCode = 603;
+      }
+    }
+
+    return repositories;
+  };
+
+  static resolveVerificationCode = (code: number) => {
+    const verificationCodeResolver: any = {
+      604: "Verificado",
+      605: "En espera",
+      606: "Aprobado",
+    };
+
+    return verificationCodeResolver[code];
+  };
+}
